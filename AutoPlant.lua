ee432419445dcfc5baf0f3d07156d374b220605d
fabuss254 <fabuss254@outlook.fr>
2021-06-04 14:53:13 +0200

    qsd

--- AutoPlant.lua
+++ AutoPlant.lua
@@ -1,1 +1,24 @@
-print("It's working!")
+function GetBlockInfos()
+    return turtle.inspect()
+end
+
+function CheckIfGrown(BlockData)
+    return BlockData.name == "minecraft:wheat" and BlockData.metadata == 7
+end
+
+function GetItemFromInventory(name, filter)
+    for i=1,12 do
+        if turtle.getItemCount(i) then
+            local tbl = turtle.getItemDetail(i)
+            print(tbl)
+        end
+    end
+end
+
+--[[
+while true do
+    
+    sleep(0.25)
+end
+]]
+GetItemFromInventory()
